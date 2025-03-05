86d679bb7761afd8bd152eda142971bab3543dae
Wendel Sabayo <wendelsabayo999@gmail.com>
2025-03-05 20:44:06 +0800

    added clerk

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,11 +4,31 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Modetoggle } from "./Modetoggle";
 import { motion } from "framer-motion";
-import { useState } from "react";
-import { Menu, X } from "lucide-react"; // For mobile menu icons
+import { useState, useEffect } from "react";
+import { Menu, X } from "lucide-react";
 import { Separator } from "./ui/separator";
+import {
+     SignedIn,
+     SignedOut,
+     SignInButton,
+     SignOutButton,
+     UserButton,
+     useUser,
+} from "@clerk/nextjs";
+import { BsCart3 } from "react-icons/bs";
 
 const Navbar = () => {
+     const { user } = useUser();
+
+     const [role, setRole] = useState<string>("");
+
+     useEffect(() => {
+          if (user) {
+               const userRole = user.publicMetadata.role as string;
+               setRole(userRole || "customer");
+          }
+     }, [user]);
+
      const [menuOpen, setMenuOpen] = useState(false);
 
      return (
@@ -24,52 +44,131 @@ const Navbar = () => {
                     }}
                     className="max-w-7xl mx-auto p-4 flex justify-between items-center"
                >
-                    {/* Logo */}
                     <h1 className="font-bold text-2xl text-emerald-400">
                          EggPress
                     </h1>
 
-                    {/* Desktop Menu */}
-                    <div className="hidden md:flex gap-2">
+                    <div className="hidden md:flex  justify-evenly items-center">
                          <Modetoggle />
+
+                         {role === "admin" && (
+                              <Link href="/admin">
+                                   <Button
+                                        variant="outline"
+                                        className="border-none"
+                                   >
+                                        Dashboard
+                                   </Button>
+                              </Link>
+                         )}
+
+                         {role === "customer" && (
+                              <Button
+                                   variant={"outline"}
+                                   className="border-none "
+                              >
+                                   <BsCart3 />
+                              </Button>
+                         )}
+                         {role !== "admin" && (
+                              <Link href="/contact">
+                                   <Button
+                                        variant="outline"
+                                        className="border-none"
+                                   >
+                                        Contact
+                                   </Button>
+                              </Link>
+                         )}
+
                          <Link href="/">
-                              <Button variant="outline">Dashboard</Button>
+                              <Button variant="outline" className="border-none mr-2">
+                                   Home
+                              </Button>
                          </Link>
-                         <Link href="/">
-                              <Button variant="outline">Contact</Button>
-                         </Link>
-                         <Button variant="destructive">Logout</Button>
+
+                         <SignedIn>
+                              <UserButton  />
+                         </SignedIn>
+
+                         <SignedOut>
+                              <SignInButton mode="modal">
+                                   <Button className="bg-sky-500 hover:bg-emerald-500 transition ease-in-out">
+                                        Sign In
+                                   </Button>
+                              </SignInButton>
+                         </SignedOut>
                     </div>
 
-                    {/* Mobile Menu Button */}
                     <button
                          className="md:hidden p-2 hover:text-sky-500 transition ease-in-out"
                          onClick={() => setMenuOpen(!menuOpen)}
                     >
-                         {menuOpen ? <X size={28} /> : <Menu size={28} />}
+                         {menuOpen ?
+                              <X size={28} />
+                         :    <Menu size={28} />}
                     </button>
                </motion.div>
 
-               {/* Mobile Menu Dropdown */}
                {menuOpen && (
                     <div className="md:hidden flex flex-col items-center gap-4 p-6 bg-background border-t">
                          <Modetoggle />
+                         {role === "customer" && (
+                              <Button
+                                   variant={"outline"}
+                                   className="border-none "
+                              >
+                                   <BsCart3 />
+                              </Button>
+                         )}
+                         {role !== "admin" && (
+                              <Link href="/contact">
+                                   <Button
+                                        variant="outline"
+                                        className="border-none"
+                                   >
+                                        Contact
+                                   </Button>
+                              </Link>
+                         )}
+
+                         <Separator className="w-40 " />
                          <Link href="/" onClick={() => setMenuOpen(false)}>
-                              <button className="hover:text-sky-500 transition ease-in-out ">
-                                   Contact
-                              </button>
+                              <Button
+                                   variant={"outline"}
+                                   className="border-none"
+                              >
+                                   Home
+                              </Button>
                          </Link>
 
                          <Separator className="w-40 " />
 
-                         <Link href="/" onClick={() => setMenuOpen(false)}>
-                              <button className="hover:text-sky-500 transition ease-in-out">
-                                   Dashboard
-                              </button>
-                         </Link>
-                         <Separator className="w-40" />
+                         {role === "admin" && (
+                              <>
+                                   <Link
+                                        href="/admin"
+                                        onClick={() => setMenuOpen(false)}
+                                   >
+                                        <button className="hover:text-sky-500 transition ease-in-out">
+                                             Dashboard
+                                        </button>
+                                   </Link>
+                                   <Separator className="w-40" />
+                              </>
+                         )}
 
-                         <Button variant="destructive">Logout</Button>
+                         <SignedIn>
+                              <SignOutButton>
+                                   <Button variant="destructive">Logout</Button>
+                              </SignOutButton>
+                         </SignedIn>
+
+                         <SignedOut>
+                              <SignInButton mode="modal">
+                                   <Button variant="default">Sign In</Button>
+                              </SignInButton>
+                         </SignedOut>
                     </div>
                )}
           </nav>
